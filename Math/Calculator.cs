499e80ed3156945fd53955914baaa0c4eb7eb97a
Rodney Chery <rchery7@wgu.edu>
2024-12-22 15:31:19 -0500

    initial commit

---
+++ Math/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpFundamentals.Math;
+
+public class Calculator
+{
+    public double Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿namespace CsharpFundamentals;
+
+public class Person
+{
+    public string FirstName;
+    public string LastName;
+
+    public void Introduce()
+    {
+        Console.WriteLine("Hello World! My name is " + FirstName + " " + LastName);
+    }
+}
