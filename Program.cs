2cf0e0f6fc0ce68704228377a3e92e3331d1f222
Rodney Chery <rchery7@wgu.edu>
2024-12-22 16:08:46 -0500

    Strings in C#

--- Program.cs
+++ Program.cs
@@ -1,28 +1,30 @@
 ﻿using System;
+using System.Runtime.InteropServices.JavaScript;
 using CsharpFundamentals.Math;
 
 namespace CsharpFundamentals
 {
-    class Program // Arrays in C#
+    class Program // Strings
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[3];
-            numbers[0] = 1;
+            var firstName = "Rodney";
+            var lastName = "Chery";
             
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            var fullName = firstName + " " + lastName;
             
-            var flags = new bool[3];
-            flags[0] = true;
+            var myFullName = string.Format("{0} {1}", firstName, lastName);
             
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+            var names = new string[3] { "Rodney", "Amos", "Chery"};
+            var formattedNames = string.Join(", ", names);
             
-            var names = new string[3] { "John", "Doe", "Jane" };
             
+            var text = @"Hi Rodney Amos Chery
+ Look into the following path
+c:\folder1\folder2\
+c:\folder3\\folder4";
+            Console.WriteLine(text);
+
         }
     }
 }
